9ad6d34254bfc96e267d87df1f17e1b288edb37b
OGMorales17 <chorly786@gmail.com>
2021-11-05 19:25:56 -0400

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Board from "./Board";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Lights Out</h2>
+        <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
